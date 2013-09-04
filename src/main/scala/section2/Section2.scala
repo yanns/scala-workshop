a84290cb1c1b4c2a3b9f26680fb14ef391079903
Yann Simon <yann.simon@epost-dev.de>
2013-09-04 14:43:35 +0200

    more simple exercices for data structures

--- src/main/scala/section2/Section2.scala
+++ src/main/scala/section2/Section2.scala
@@ -2,9 +2,21 @@ package section2
 
 object Section2 {
 
+  // Arrays
+
+  def loud(input: Array[String]): Array[String] = ???
+
   def concatenate(input: Array[String]): String = ???
 
+  // Seq and List
+
+  def loud(input: Seq[String]): Seq[String] = ???
+
   def extractWords(input: Seq[String]): Seq[String] = ???
+
+  // Map
+
+  def loud(input: Map[String, String]): Map[String, String] = ???
 
   def counts(input: Seq[String]): Map[String, Int] = ???
 
--- src/test/scala/section2/Section2Spec.scala
+++ src/test/scala/section2/Section2Spec.scala
@@ -4,14 +4,35 @@ import org.scalatest.FunSuite
 
 class Section2Spec extends FunSuite {
 
+  // Arrays
+
+  test("Section2 can speak very loud") {
+    val result = Section2.loud(Array("Hi", "there"))
+    assert(result === Array("HI", "THERE"))
+  }
+
   test("Section2 concatenates Strings together") {
     val result = Section2.concatenate(Array("Scala", "is", "fun"))
     assert(result === "Scala is fun")
   }
 
+  // Seq and List
+
+  test("Section2 can speak very loud with Seq") {
+    val result = Section2.loud(Seq("Hi", "there"))
+    assert(result === Seq("HI", "THERE"))
+  }
+
   test("Section2 extract words from list from Strings") {
     val result = Section2.extractWords(Seq("Scala is fun", "fun is good"))
     assert(result === Seq("Scala", "is", "fun", "fun", "is", "good"))
+  }
+
+  // Map
+
+  test("Section2 can speak very loud with Map") {
+    val result = Section2.loud(Map("Hi" -> "there", "hello" -> "world"))
+    assert(result === Map("HI" -> "THERE", "HELLO" -> "WORLD"))
   }
 
   test("Section2 counts number of occurrences of words") {
