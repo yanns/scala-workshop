70dad816a8824ea1dfa66585ea39645d32b67771
Yann Simon <yann.simon.fr@gmail.com>
2014-02-12 08:49:11 +0100

    use seq

--- src/main/scala/section10/Section10.scala
+++ src/main/scala/section10/Section10.scala
@@ -24,7 +24,7 @@ object Section10 {
     """.stripMargin
 
 
-  val words = text.replace(","," ").replace(".", " ").replace("\n", " ").replace("(", "").replace(")","").replaceAll("\\s", " ").split(" ").toVector
+  val words = text.replace(","," ").replace(".", " ").replace("\n", " ").replace("(", "").replace(")","").split(" ").toSeq
 
   def wordsWithoutEmptyString: Seq[String] = ???
 
