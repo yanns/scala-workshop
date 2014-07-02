94aef76be971095d93d9247b9b8b62db7faab841
Yann Simon <yann.simon.fr@gmail.com>
2014-07-02 11:08:43 +0200

    update sbt to 0.13.5

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,2 @@
 set SCRIPT_DIR=%~dp0
-java %SBT_OPTS% -Dfile.encoding=UTF-8 -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%\sbtwrapper\sbt-launch-0.13.2.jar" %*
+java %SBT_OPTS% -Dfile.encoding=UTF-8 -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%\sbtwrapper\sbt-launch-0.13.5.jar" %*
