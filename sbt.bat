2c3356a69eaf53b80d4f2490f2690615a899896d
Matthias Erche <matthias.erche@epost-dev.de>
2015-07-19 19:39:42 +0200

    Remove sbt scripts because activator is used now

--- sbt.bat
+++
@@ -1,2 +0,0 @@
-set SCRIPT_DIR=%~dp0
-java %SBT_OPTS% -Dfile.encoding=UTF-8 -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%\sbtwrapper\sbt-launch-0.13.7.jar" %*
