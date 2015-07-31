7bf514b1e4371a30a9bf34a078f4f9c4b0d465e1
Yann Simon <yann.simon@commercetools.de>
2015-07-31 12:33:38 +0200

    actor

---
+++ src/main/scala/section7/Actor.scala
@@ -0,0 +1,38 @@
+package section7
+
+import akka.actor.Actor.Receive
+import akka.actor.{Props, Actor, ActorSystem}
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+import scala.concurrent.{Await, Future}
+
+object Actor extends App {
+
+  val system = ActorSystem()
+
+  class EchoServer extends Actor {
+    var nbrOfMessage = 0
+    override def receive: Receive = {
+      case "how much?" => sender() ! nbrOfMessage
+      case msg: String =>
+        nbrOfMessage += 1
+        println(s"[$nbrOfMessage] echo $msg")
+    }
+  }
+
+  val echoServer = system.actorOf(Props[EchoServer])
+  echoServer ! "hi"
+  echoServer ! "wie geht's"
+  echoServer ! "wie geht's"
+  echoServer ! "wie geht's"
+  echoServer ! "wie geht's"
+
+  implicit val timeout: Timeout = 1 second
+  val result = (echoServer ? "how much?").asInstanceOf[Future[Int]]
+
+  println(Await.result(result, 2 seconds))
+
+  system.shutdown()
+}
